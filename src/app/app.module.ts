a4ba0e1df7fbc7aaf1e68bbbf7722706ff8659f3
PavloZaluskyy <pavlozaluskiy@gmail.com>
2024-11-16 14:49:09 +0200

    Create new project and develop a few pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { routes } from './app.routes';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    RouterOutlet,
+    RouterModule.forRoot(routes),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { FinancesComponent } from './components/finances/finances.component';
+
+export const routes: Routes = [
+    // {path: "finances", component: FinancesComponent}
+    {path: "", component: FinancesComponent}
+];
