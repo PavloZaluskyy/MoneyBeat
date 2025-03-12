44baaae1e2e3d81acf33731b3d0f57275ebb697b
PavloZaluskyy <pavlozaluskiy@gmail.com>
2025-03-12 20:05:57 +0200

    [bugs ticket 001] Create PWA manifest json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -51,6 +51,7 @@ import { EditReceiptComponent } from './components/edit-receipt/edit-receipt.com
 import { SettingEarningComponent } from './components/setting-earning/setting-earning.component';
 import { EditEarningComponent } from './components/edit-earning/edit-earning.component';
 import { ManualComponent } from './components/manual/manual.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 // import { environments } from '../environments/environments';
 @NgModule({
   declarations: [
@@ -102,7 +103,13 @@ import { ManualComponent } from './components/manual/manual.component';
     MatFormFieldModule,
     NgbAccordionModule,
     NgbDropdownModule,
-    NgbScrollSpyModule
+    NgbScrollSpyModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   
     // CalendarModule.forRoot({
     //   provide: DateAdapter,
